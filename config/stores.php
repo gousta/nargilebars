f584e08caa9b34424b4c623babded297d505285f
Stratos Giouldasis <giouldasis.stratos@gmail.com>
2017-02-04 01:55:51 +0200

    Added 30 new stores

--- config/stores.php
+++ config/stores.php
@@ -19,7 +19,7 @@ return [
                 'street' => 'Αγίας Γλυκερίας 20',
                 'area' => 'Γαλάτσι',
                 'region' => 'Αττική',
-                'zip' => '111 46'
+                'zip' => '11146'
             ],
             'phone' => '2102136155',
             'icon' => 'default.png'
@@ -40,7 +40,7 @@ return [
                 'street' => 'Δωδεκανήσου 47',
                 'area' => 'Νίκαια Άγιος Ιωάννης Ρέντης',
                 'region' => 'Αττική',
-                'zip' => '184 52'
+                'zip' => '18452'
             ],
             'phone' => '21041171911',
             'icon' => 'default.png'
@@ -61,7 +61,7 @@ return [
                 'street' => 'Πλατεία Παγκόσμιας Ειρήνης 3',
                 'area' => 'Κορυδαλλός',
                 'region' => 'Αττική',
-                'zip' => '181 20'
+                'zip' => '18120'
             ],
             'phone' => '2104955544',
             'icon' => 'default.png'
@@ -82,7 +82,7 @@ return [
                 'street' => 'Σειρήνων 37',
                 'area' => 'Παλαιό Φάληρο',
                 'region' => 'Αττική',
-                'zip' => '175 61'
+                'zip' => '17561'
             ],
             'phone' => '2112137597',
             'icon' => 'default.png'
@@ -103,7 +103,7 @@ return [
                 'street' => 'Αγίας Ελεούσης 2',
                 'area' => 'Καλλιθέα',
                 'region' => 'Αττική',
-                'zip' => '176 75'
+                'zip' => '17675'
             ],
             'phone' => '2109581400',
             'icon' => 'default.png'
@@ -124,7 +124,7 @@ return [
                 'street' => 'Ακτή Κουμουνδούρου 20',
                 'area' => 'Πειραιάς',
                 'region' => 'Αττική',
-                'zip' => '185 33'
+                'zip' => '18533'
             ],
             'phone' => '2104127169',
             'icon' => 'default.png'
@@ -145,7 +145,7 @@ return [
                 'street' => 'Μεγαλουπόλεως 1',
                 'area' => 'Καλλιθέα',
                 'region' => 'Αττική',
-                'zip' => '176 75'
+                'zip' => '17675'
             ],
             'phone' => '2109480501',
             'icon' => 'default.png'
@@ -166,7 +166,7 @@ return [
                 'street' => 'Πεισίστρατου 87',
                 'area' => 'Καλλιθέα',
                 'region' => 'Αττική',
-                'zip' => '176 74'
+                'zip' => '17674'
             ],
             'phone' => '2109407720',
             'icon' => 'default.png'
@@ -187,7 +187,7 @@ return [
                 'street' => 'Κωνσταντίνου Καβάφη 14',
                 'area' => 'Αθήνα',
                 'region' => 'Αττική',
-                'zip' => '111 43'
+                'zip' => '11143'
             ],
             'phone' => '',
             'icon' => 'default.png'
@@ -208,7 +208,7 @@ return [
                 'street' => '2ας Μαΐου 9',
                 'area' => 'Νέα Σμύρνη',
                 'region' => 'Αττική',
-                'zip' => '171 21'
+                'zip' => '17121'
             ],
             'phone' => '2109327400',
             'icon' => 'default.png'
@@ -229,7 +229,7 @@ return [
                 'street' => 'Λεωφόρος Στρατάρχου Αλεξάνδρου Παπάγου 127',
                 'area' => 'Ζωγράφου',
                 'region' => 'Αττική',
-                'zip' => '157 73'
+                'zip' => '15773'
             ],
             'phone' => '2107706003',
             'icon' => 'default.png'
@@ -250,7 +250,7 @@ return [
                 'street' => 'Μιαούλη 21',
                 'area' => 'Αθήνα',
                 'region' => 'Αττική',
-                'zip' => '105 54'
+                'zip' => '10554'
             ],
             'phone' => '2103239370',
             'icon' => 'default.png'
@@ -271,16 +271,12 @@ return [
                 'street' => 'Καραϊσκάκη 10',
                 'area' => 'Αθήνα',
                 'region' => 'Αττική',
-                'zip' => '105 54'
+                'zip' => '10554'
             ],
             'phone' => '2103317293',
             'icon' => 'default.png'
         ],
-
-
-        //----------------------------------------------------------------------
-        // Xilies kai dio nyxtes
-        //----------------------------------------------------------------------
+        
         [
             'key' => 'pyramides3',
             'position' => [
@@ -296,6 +292,474 @@ return [
                 'zip' => '187 56'
             ],
             'phone' => '2104619798',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'royalshisha',
+            'position' => [
+                'lat' => '37.97657',
+                'lng' => '23.77343'
+            ],
+            'title' => 'Royal Shisha',
+            'content' => '',
+            'address' => [
+                'street' => 'Λεωφόρος Στρατάρχου Αλεξάνδρου Παπάγου 118',
+                'area' => 'Ζωγράφου',
+                'region' => 'Αττική',
+                'zip' => '157 72'
+            ],
+            'phone' => '2107455645',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'goldenhole',
+            'position' => [
+                'lat' => '37.95886',
+                'lng' => '23.62198'
+            ],
+            'title' => 'Golden Hole',
+            'content' => '',
+            'address' => [
+                'street' => 'Λεωφόρος Δεκελείας 61',
+                'area' => 'Νέα Χαλκηδόνα',
+                'region' => 'Αττική',
+                'zip' => '14343'
+            ],
+            'phone' => '2107455645',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'shishabyfaraon',
+            'position' => [
+                'lat' => '37.95886',
+                'lng' => '23.62198'
+            ],
+            'title' => 'Shisha By Faraon',
+            'content' => '',
+            'address' => [
+                'street' => 'Έβρου 52',
+                'area' => 'Κερατσίνι',
+                'region' => 'Αττική',
+                'zip' => '18756'
+            ],
+            'phone' => '2107455645',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'nefertiti',
+            'position' => [
+                'lat' => '37.97304',
+                'lng' => '23.65297'
+            ],
+            'title' => 'Nefertiti',
+            'content' => '',
+            'address' => [
+                'street' => 'Αβέρωφ 21',
+                'area' => 'Νίκαια',
+                'region' => 'Αττική',
+                'zip' => '18120'
+            ],
+            'phone' => '2104971621',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'lyxnaricafe',
+            'position' => [
+                'lat' => '37.97281',
+                'lng' => '23.64789'
+            ],
+            'title' => 'Λυχνάρι Cafe',
+            'content' => '',
+            'address' => [
+                'street' => 'Αβέρωφ 87',
+                'area' => 'Κορυδαλλος',
+                'region' => 'Αττική',
+                'zip' => '18120'
+            ],
+            'phone' => '2104979038',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'arodou',
+            'position' => [
+                'lat' => '37.9779603',
+                'lng' => '23.7230692'
+            ],
+            'title' => 'Αρόδου',
+            'content' => '',
+            'address' => [
+                'street' => 'Μιαούλη 22',
+                'area' => 'Αθήνα',
+                'region' => 'Αττική',
+                'zip' => '18120'
+            ],
+            'phone' => '2103216774',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'habibi',
+            'position' => [
+                'lat' => '37.935187',
+                'lng' => '23.7094025'
+            ],
+            'title' => 'Habibi',
+            'content' => '',
+            'address' => [
+                'street' => 'Λεωφόρος Ελελεύθεριου Βενιζέλου 181',
+                'area' => 'Νέα Σμύρνη',
+                'region' => 'Αττική',
+                'zip' => '17123'
+            ],
+            'phone' => '2109333213',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'sahten',
+            'position' => [
+                'lat' => '37.9549481',
+                'lng' => '23.6400976'
+            ],
+            'title' => 'Sahten',
+            'content' => '',
+            'address' => [
+                'street' => 'Λακωνίας 1',
+                'area' => 'Πειραιάς',
+                'region' => 'Αττική',
+                'zip' => '18545'
+            ],
+            'phone' => '2114076516',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'qassabahloungecafe',
+            'position' => [
+                'lat' => '37.9763562',
+                'lng' => '23.7743643'
+            ],
+            'title' => 'Qassabah Lounge Cafe',
+            'content' => '',
+            'address' => [
+                'street' => 'Χερσικρατους 8',
+                'area' => 'Ζωγράφου',
+                'region' => 'Αττική',
+                'zip' => '15773'
+            ],
+            'phone' => '6907194317',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'anatolian-lounge-hookaholic',
+            'position' => [
+                'lat' => '37.9797223',
+                'lng' => '23.7097713'
+            ],
+            'title' => 'Anatolian Lounge - Hookaholic',
+            'content' => '',
+            'address' => [
+                'street' => 'Ευμολπιδών 33',
+                'area' => 'Αθήνα',
+                'region' => 'Αττική',
+                'zip' => '11854'
+            ],
+            'phone' => '2103464222',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'aroma',
+            'position' => [
+                'lat' => '37.9688451',
+                'lng' => '23.7504676'
+            ],
+            'title' => 'Άρωμα',
+            'content' => '',
+            'address' => [
+                'street' => 'Υμητού 67',
+                'area' => 'Αθήνα',
+                'region' => 'Αττική',
+                'zip' => '11633'
+            ],
+            'phone' => '2155515690',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'sahara',
+            'position' => [
+                'lat' => '37.951805',
+                'lng' => '23.6939607'
+            ],
+            'title' => 'Sahara',
+            'content' => '',
+            'address' => [
+                'street' => 'Μεταμορφώσεως 3',
+                'area' => 'Καλλιθέα',
+                'region' => 'Αττική',
+                'zip' => '17673'
+            ],
+            'phone' => '2109590091',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'alfaraana',
+            'position' => [
+                'lat' => '37.9484483',
+                'lng' => '23.7339555'
+            ],
+            'title' => 'Al Faraana',
+            'content' => '',
+            'address' => [
+                'street' => 'Λεωφόρος Αλεξάνδρου Παπαναστασίου 14',
+                'area' => 'Άγιος Δημήτριος',
+                'region' => 'Αττική',
+                'zip' => '17235'
+            ],
+            'phone' => '2109751646',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'relax-cafe',
+            'position' => [
+                'lat' => '37.9407347',
+                'lng' => '23.6441003'
+            ],
+            'title' => 'Relax Cafe',
+            'content' => '',
+            'address' => [
+                'street' => 'Καραϊσκου 145',
+                'area' => 'Πειραιάς',
+                'region' => 'Αττική',
+                'zip' => '18535'
+            ],
+            'phone' => '2104296956',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'golden-cafe',
+            'position' => [
+                'lat' => '37.9407347',
+                'lng' => '23.6441003'
+            ],
+            'title' => 'Golden Cafe',
+            'content' => '',
+            'address' => [
+                'street' => 'Λεωφόρος Ιωνίας 178',
+                'area' => 'Αθήνα',
+                'region' => 'Αττική',
+                'zip' => '11144'
+            ],
+            'phone' => '2110123008',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'nargile',
+            'position' => [
+                'lat' => '38.0748563',
+                'lng' => '23.8192325'
+            ],
+            'title' => 'Nargile',
+            'content' => '',
+            'address' => [
+                'street' => 'Χαριλάου Τρικούπη 50',
+                'area' => 'Κηφισιά',
+                'region' => 'Αττική',
+                'zip' => '14562'
+            ],
+            'phone' => '2108083333',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'king-of-nargile',
+            'position' => [
+                'lat' => '37.9830486',
+                'lng' => '23.6533061'
+            ],
+            'title' => 'King of Nargile',
+            'content' => '',
+            'address' => [
+                'street' => 'Πελοποννήσου 109',
+                'area' => 'Κορυδαλλός',
+                'region' => 'Αττική',
+                'zip' => '18121'
+            ],
+            'phone' => '2110121223',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'pharaon-shisha-bar',
+            'position' => [
+                'lat' => '37.9322167',
+                'lng' => '23.6950611'
+            ],
+            'title' => 'Pharaon Shisha Bar',
+            'content' => '',
+            'address' => [
+                'street' => 'Συνταγματάρχου Ζησιμοπούλου 107',
+                'area' => 'Παλαιό Φάληρο',
+                'region' => 'Αττική',
+                'zip' => '17561'
+            ],
+            'phone' => '6981996383',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'minas-nargiles',
+            'position' => [
+                'lat' => '38.0117626',
+                'lng' => '23.7023534'
+            ],
+            'title' => 'Minas Nargiles',
+            'content' => '',
+            'address' => [
+                'street' => '',
+                'area' => 'Παρασκευπούλου 44',
+                'region' => 'Αττική',
+                'zip' => '12132'
+            ],
+            'phone' => '6931175664',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'alladin-nargile-cafe',
+            'position' => [
+                'lat' => '38.0117626',
+                'lng' => '23.7023534'
+            ],
+            'title' => 'Alladin Nargile Cafe',
+            'content' => '',
+            'address' => [
+                'street' => 'Κώστα Βάρναλη 6',
+                'area' => 'Πέραμα',
+                'region' => 'Αττική',
+                'zip' => '18863'
+            ],
+            'phone' => '2104022676',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'al-sokareia',
+            'position' => [
+                'lat' => '37.9518581',
+                'lng' => '23.6932279'
+            ],
+            'title' => 'Al Sokareia',
+            'content' => '',
+            'address' => [
+                'street' => 'Λεωφόρος Ελελεύθεριου Βενιζέλου 237',
+                'area' => 'Καλλιθέα',
+                'region' => 'Αττική',
+                'zip' => '17673'
+            ],
+            'phone' => '2109560026',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'new-dubai-cafe',
+            'position' => [
+                'lat' => '37.9772743',
+                'lng' => '23.7111772',
+            ],
+            'title' => 'New Dubai Cafe',
+            'content' => '',
+            'address' => [
+                'street' => 'Ορφέως 20 & Τριπτολέμου 43',
+                'area' => 'Αθήνα',
+                'region' => 'Αττική',
+                'zip' => '11845'
+            ],
+            'phone' => '2103464476',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'o-nargiles-tou-ali-baba',
+            'position' => [
+                'lat' => '37.9537353',
+                'lng' => '23.6541928',
+            ],
+            'title' => 'Ο ναργιλές του Ali Baba',
+            'content' => '',
+            'address' => [
+                'street' => 'Χίου 3',
+                'area' => 'Πειραιάς',
+                'region' => 'Αττική',
+                'zip' => '18541'
+            ],
+            'phone' => '6976166794',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'alexandria-cafe',
+            'position' => [
+                'lat' => '37.9537353',
+                'lng' => '23.6541928',
+            ],
+            'title' => 'Alexandria Cafe',
+            'content' => '',
+            'address' => [
+                'street' => 'Σκρά 34',
+                'area' => 'Καλλιθέα',
+                'region' => 'Αττική',
+                'zip' => '17673'
+            ],
+            'phone' => '2109521113',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'maya-lounge-cafe',
+            'position' => [
+                'lat' => '37.8640631',
+                'lng' => '23.7469827',
+            ],
+            'title' => 'Maya Lounge Cafe',
+            'content' => '',
+            'address' => [
+                'street' => 'Μαραγκού 8',
+                'area' => 'Γλυφάδα',
+                'region' => 'Αττική',
+                'zip' => '16675'
+            ],
+            'phone' => '2108980467',
+            'icon' => 'default.png'
+        ],
+
+        [
+            'key' => 'aroma-anatolis',
+            'position' => [
+                'lat' => '37.9356395',
+                'lng' => '23.6970295',
+            ],
+            'title' => 'Άρωμα Ανατολής',
+            'content' => '',
+            'address' => [
+                'street' => 'Συνταγματάρχου Ζησιμοπούλου 69',
+                'area' => 'Παλαιό Φάληρο',
+                'region' => 'Αττική',
+                'zip' => '17564'
+            ],
+            'phone' => '2109404424',
             'icon' => 'default.png'
         ],
     ]
